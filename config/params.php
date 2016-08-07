6b193c6672d07e7f5bb2ce10b16f6a97a43f0348
Александр Шахмухаметов <diablus2012@gmail.com>
2016-08-07 17:17:53 +0500

    WIP

--- config/params.php
+++ config/params.php
@@ -1,13 +1,14 @@
 <?php
-
+$protocol = 'http';
+$host = $_SERVER['HTTP_HOST'];
 return [
     'adminEmail' => 'ru-staff@ivao.aero',
 	'twitterLink' => '',
 	'fbLink' => '',
 	'vkLink' => '',
 	'divisionSite' => '',
-	'adminsvids' => [439914],
+	'adminsvids' => [439914,350631,211774,334668,442121,472469],
     'languages' => ['ru'=>'Русский','en'=>'English'],
-    'api_url' => 'https://login.ivao.aero/index.php?url=http://localhost.va-transaero.ru:8098/site/login',
+    'api_url' => 'https://login.ivao.aero/index.php?url='.$protocol.'://'.$host.'/site/login',
 	'installed' => true,
 ];
